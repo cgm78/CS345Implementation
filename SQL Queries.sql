eed190fe176679669542b8c616b5d3881e0873d6
JevinD <32504861+JevinD@users.noreply.github.com>
2019-04-24 22:01:42 -0700

    2nd Query I need help displaying the businesses name

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -3,3 +3,7 @@ SELECT c.customerfirstname,c.customerlastname,c.customerphonenum
 FROM customer c, transaction t
 WHERE  t.productid = 'HA' AND t.customerid = c.customerid;
 
+#finding the ammount of shareholders for a business
+SELECT businessid,count(*) 
+FROM  shares
+GROUP BY businessid;
