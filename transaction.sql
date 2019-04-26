f179947d8403ee1a439b37d721e5a195d68b02af
cgm78 <42878615+cgm78@users.noreply.github.com>
2019-04-25 22:14:40 -0700

    Edited database below uses transaction line.
    Database should be created in full when this code is run.

--- transaction.sql
+++ transaction.sql
@@ -12,7 +12,7 @@ CREATE TABLE customer (
    customerfirstname VARCHAR(25) NOT NULL,
    customerPhoneNum VARCHAR(10) NULL,
    PRIMARY KEY (customerid));
-   
+
 CREATE TABLE customerPhone (
    customerid CHAR(2) NOT NULL,
    customerPhoneNum VARCHAR(10) NULL,
@@ -23,11 +23,11 @@ CREATE TABLE business (
    businessname VARCHAR(25) NOT NULL,
    PRIMARY KEY (businessid));
 
-CREATE TABLE transaction (
-   transactionid INT(5) NOT NULL,
-   productid CHAR(5) NOT NULL,
-   businessid CHAR(5) NOT NULL,
-   customerid CHAR(5) NOT NULL,
+CREATE TABLE transactions (
+   transactionid INT NOT NULL,
+   productid CHAR(2) NOT NULL,
+   businessid CHAR(3) NOT NULL,
+   customerid CHAR(2) NOT NULL,
    PRIMARY KEY(transactionid),
    FOREIGN KEY(productid) REFERENCES product(productid),
    FOREIGN KEY(businessid) REFERENCES business(businessid),
@@ -54,7 +54,7 @@ CREATE TABLE supplies (
 CREATE TABLE supplier (
    supplierid CHAR(3) NOT NULL,
    suppliername VARCHAR(25) NOT NULL,
-   supplycategory VARCHAR(25) NOT NULL,
+   supplycategory CHAR(3) NOT NULL,
    PRIMARY KEY (supplierid),
    FOREIGN KEY(supplycategory) REFERENCES supplies(supplycategory));
 
@@ -82,16 +82,16 @@ INSERT INTO customerPhone VALUES ('B1', '3237889901');
 INSERT INTO customerPhone VALUES ('C1', '3235545542');
 
 /*BUSINESS TABLE*/
-INSERT INTO business VALUES ('111', "Terry's Tools");
-INSERT INTO business VALUES ('222', "Cornell's Corks");
-INSERT INTO business VALUES ('333', "Walter's Wines");
+INSERT INTO business VALUES ('111', 'Terry''s Tools');
+INSERT INTO business VALUES ('222', 'Cornell''s Corks');
+INSERT INTO business VALUES ('333', 'Walter''s Wines');
 
 /*SHAREHOLDERS TABLE*/
-INSERT INTO shareholders VALUES ('AAA', "Ivan's Investments");
+INSERT INTO shareholders VALUES ('AAA', 'Ivan''s Investments');
 INSERT INTO shareholders VALUES ('BBB', 'Thomas Jane');
-INSERT INTO shareholders VALUES ('CCC', "Fred's Funding");
-INSERT INTO shareholders VALUES ('DDD', "John's Banking");
-INSERT INTO shareholders VALUES ('EEE', "Jared's Construction");
+INSERT INTO shareholders VALUES ('CCC', 'Fred''s Funding');
+INSERT INTO shareholders VALUES ('DDD', 'John''s Banking');
+INSERT INTO shareholders VALUES ('EEE', 'Jared''s Construction');
 
 /*SHARES TABLE
 *References shareholderid and businessid*/
@@ -104,21 +104,21 @@ INSERT INTO shares VALUES ('A06', 'EEE', '111');
 
 /*SUPPLIES TABLE
 *'FOO' = food, 'TOO' = tools, "ELE" = electronics*/
-INSERT INTO supplies VALUES ('FOO', "Bob's Boys");
-INSERT INTO supplies VALUES ('TOO', "Perry's People");
-INSERT INTO supplies VALUES ('ELE', "Momma's Movers");
+INSERT INTO supplies VALUES ('FOO', 'Bob''s Boys');
+INSERT INTO supplies VALUES ('TOO', 'Perry''s People');
+INSERT INTO supplies VALUES ('ELE', 'Momma''s Movers');
 
 /*SUPPLIER TABLE*/
-INSERT INTO supplier VALUES ('000', "Sally's Supplies", 'FOO');
-INSERT INTO supplier VALUES ('001', "Emie's Essentials", 'TOO');
-INSERT INTO supplier VALUES ('002', "Neil's Necessities", 'ELE');
+INSERT INTO supplier VALUES ('000', 'Sally''s Supplies', 'FOO');
+INSERT INTO supplier VALUES ('001', 'Emie''s Essentials', 'TOO');
+INSERT INTO supplier VALUES ('002', 'Neil''s Necessities', 'ELE');
 
 /*TRANSACTION TABLE
 *References productid, businessid, and customerid*/
-INSERT INTO transaction VALUES (67890, 'CY', '111', 'A1');
-INSERT INTO transaction VALUES (12345, 'DH', '222', 'B1');
-INSERT INTO transaction VALUES (10011, 'HA', '333', 'C1');
-INSERT INTO transaction VALUES (12346, 'CH', '222', 'C1');
+INSERT INTO transactions VALUES (67890, 'CY', '111', 'A1');
+INSERT INTO transactions VALUES (12345, 'DH', '222', 'B1');
+INSERT INTO transactions VALUES (10011, 'HA', '333', 'C1');
+INSERT INTO transactions VALUES (12346, 'CH', '222', 'C1');
 
 
 /*WHOLESALE TABLE
